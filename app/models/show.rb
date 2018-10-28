5b7dc94527833c6c54ef685d0dceda40abf3f6a0
Brian Birdwell <bbirdwell@gmail.com>
2018-10-28 05:03:54 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,14 @@
 class Show < ActiveRecord::Base
 
   def highest_rating
-    show = Show.order(rating)
+    show = Show.order(rating: :desc).take
   end
   
   def most_popular_show
+    show = Show.order(season: :desc).take
   end
 
   def lowest_rating
+    show = Show.order(rating: :asc)
   end
 end
